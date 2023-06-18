f8c2e471eff2767d2557e5b9b50c3a31e782f5e0
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-06-18 13:56:33 -0400

    Deploy test

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -6,54 +6,52 @@ function CardExp(props) {
 
   if (props.openSpots === 0) {
     badgeTex = "SOLD OUT";
-  }
-  
-  else if (props.location === "Online") {
+  } else if (props.location === "Online") {
     badgeTex = "Online";
   }
 
   return (
-    <div className="flex-grow-0 flex-shrink-0 basis-auto relative">
-      <img
-        src={props.coverImg}
-        className="rounded-[9px] w-[176px] h-[235px] object-fill"
-        alt="experience-1"
-      />
-      <div className="text-[12px]">
-        <div className="inline-flex items-center mt-[10px]">
-          <img
-            src={star}
-            className="inline-block mr-1 translate-y-[-11%]"
-            alt="star"
-          />
-          <p>{props.stats.rating}</p>
-          <span className="text-[#918E9B] ml-1">
-            ({props.stats.reviewCount})
-          </span>
-          <span className="text-[#918E9B] ml-1">• {props.location}</span>
+    <a href={"#"}>
+      <div className="flex-grow-0 flex-shrink-0 basis-auto relative">
+        <img
+          src={props.coverImg}
+          className="rounded-[9px] w-[176px] h-[235px] object-fill"
+          alt="experience-1"
+        />
+        <div className="text-[12px]">
+          <div className="inline-flex items-center mt-[10px]">
+            <img
+              src={star}
+              className="inline-block mr-1 translate-y-[-11%]"
+              alt="star"
+            />
+            <p>{props.stats.rating}</p>
+            <span className="text-[#918E9B] ml-1">
+              ({props.stats.reviewCount})
+            </span>
+            <span className="text-[#918E9B] ml-1">• {props.location}</span>
+          </div>
+          <p className="text-[#222]">{props.title}</p>
+          <p>
+            <b className="font-semibold">From ${props.price}</b> / person
+          </p>
         </div>
-        <p className="text-[#222]">{props.title}</p>
-        <p>
-          <b className="font-semibold">From ${props.price}</b> / person
-        </p>
+        {badgeTex && (
+          <span className="text-[10.24px] bg-white rounded-sm absolute top-[6px] left-[6px] px-[6px] py-1">
+            {badgeTex}
+          </span>
+        )}
       </div>
-      {badgeTex && (
-        <span className="text-[10.24px] bg-white rounded-sm absolute top-[6px] left-[6px] px-[6px] py-1">
-          {badgeTex}
-        </span>
-      )}
-    </div>
+    </a>
   );
 }
 
 function CardSection() {
-  const experiences = expData.map((item) => {
-    return <CardExp key={item.id} {...item} />;
-  });
-
   return (
     <section className="flex gap-5 mt-7 container mx-auto mb-20 px-10 overflow-auto">
-      {experiences}
+      {expData.map((item) => {
+        return <CardExp key={item.id} num={++count} {...item}/>;
+      })}
     </section>
   );
 }
--- src/components/Challengue.jsx
+++ src/components/Challengue.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Challengue({img, name, phoneNum, email}) {
 
   return (
